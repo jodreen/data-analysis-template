fcd1de9f513761743f289051c33b2d4f52c56ed0
Jordeen Chang <jordeenchang@gmail.com>
2014-05-04 18:11:37 -0700

    filling out notebook 4 with data cleaning part

--- website/publishconf.py
+++ website/publishconf.py
@@ -10,14 +10,11 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-<<<<<<< HEAD
 ####################################################################
 # URL of the github repository website associated with the fork
 # of this repository  (Replace accordingly)
 ####################################################################
-=======
 SITEURL = 'http://jodreen.github.io/googletrends-stocks/'
->>>>>>> 6db6961d43b2c2c991b7c5c115b5ad1e642751ac
 
 RELATIVE_URLS = False
 
